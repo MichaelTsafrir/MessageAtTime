a1622d56a245ea916df17e0136986ca94cb33d5f
Michael Tsafrir <michaeltsafrir@gmail.com>
2019-12-10 02:52:45 +0200

    added post and body parser

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import express from "express";
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5040;
 
-app.get('/', (req, res) => res.send('hello world'));
+// Use bodyParser Middleware to fetch body params
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/addMessage', (req, res) => {
+    const message = req.body.message;
+    const time = req.body.time;
+    
+
+    res.send(`got ${message} and ${time}`);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-console.log('hello world');
