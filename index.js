1a8eacb1351140c4bdccfc04f2b4c52bb8b5d4d1
Michael Tsafrir <michaeltsafrir@gmail.com>
2019-12-11 04:02:00 +0200

    switched to ioredis and async await in addMessage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const redis = require('redis');
+const ioredis = require('ioredis');
 const fetchConfig = require('zero-config');
 
 const config = fetchConfig(__dirname, {});
@@ -12,63 +12,63 @@ const messageList = config.get("redis.messageList") || "messageList";
 const messageKey = config.get("redis.messageKey") || "messageKey";
 
 const app = express();
-const client = redis.createClient(redisPort);
+const client = new ioredis(redisPort, redisHost);
 
 // Send an error response
 const responseError = (response, message) => response.send(JSON.stringify({ status: 'error', message }));
 
 // Get the earliest message that exists
-const fetchMessage = () => client.zrange(messageList, 0, 0, 'withscores', (err, messageSet) => {
-    if (err) {
-        return console.error(`Redis: error when fetching first element from "${messageList}". Error: ${err}`);
-    }
-    else if (!messageSet.length) {
-        // No message fetched
-        return;
-    }
-    else {
-        // Fetched message
-        const messageID = messageSet[0];
-        const messageTime = parseInt(messageSet[1]);
-        const now = Date.now() / 1000; // Get epoch time in seconds
+// const fetchMessage = () => client.zrange(messageList, 0, 0, 'withscores', (err, messageSet) => {
+//     if (err) {
+//         return console.error(`Redis: error when fetching first element from "${messageList}". Error: ${err}`);
+//     }
+//     else if (!messageSet.length) {
+//         // No message fetched
+//         return;
+//     }
+//     else {
+//         // Fetched message
+//         const messageID = messageSet[0];
+//         const messageTime = parseInt(messageSet[1]);
+//         const now = Date.now() / 1000; // Get epoch time in seconds
 
-        if (messageTime <= now) {
-            client.get(messageID, (err, message) => {
-                if (err) {
-                    return console.error(`Redis: error when fetching "${messageID}". Error: ${err}`);
-                }
-                else {
-                    console.log(message);
+//         if (messageTime <= now) {
+//             client.get(messageID, (err, message) => {
+//                 if (err) {
+//                     return console.error(`Redis: error when fetching "${messageID}". Error: ${err}`);
+//                 }
+//                 else {
+//                     console.log(message);
 
-                    // Delete message
-                    client.zrem(messageList, messageID, (err, wasRemoved) => {
-                        if (err || !wasRemoved) {
-                            console.error(`Redis: Couldn't delete "${messageID}" from "${messageList}". Error: ${err}`);
-                        }
-                        else {
-                            client.del(messageID, (err, wasRemoved) => {
-                                if (err || !wasRemoved) {
-                                    console.error(`Redis: Couldn't delete "${messageID}". Error: ${err}`);
-                                }
-                            });
-                        }
-                    });
+//                     // Delete message
+//                     client.zrem(messageList, messageID, (err, wasRemoved) => {
+//                         if (err || !wasRemoved) {
+//                             console.error(`Redis: Couldn't delete "${messageID}" from "${messageList}". Error: ${err}`);
+//                         }
+//                         else {
+//                             client.del(messageID, (err, wasRemoved) => {
+//                                 if (err || !wasRemoved) {
+//                                     console.error(`Redis: Couldn't delete "${messageID}". Error: ${err}`);
+//                                 }
+//                             });
+//                         }
+//                     });
 
-                    // Check next message
-                    return fetchMessage();
-                }
-            });
-        }
-    }
-});
+//                     // Check next message
+//                     return fetchMessage();
+//                 }
+//             });
+//         }
+//     }
+// });
 
 // Run from the earliest message that exists
-fetchMessage();
+// fetchMessage();
 
 // Use bodyParser Middleware to fetch body params
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/addMessage', (req, res) => {
+app.post('/addMessage', async (req, res) => {
     const message = req.body.message;
     const time = req.body.time;
 
@@ -80,32 +80,34 @@ app.post('/addMessage', (req, res) => {
     }
     else {
         // Get the current last key (will be created if doesn't exists)
-        client.incr(messageKey, (err, num) => {
-            if (err) {
-                responseError(res, `Redis: couldn't increment ${messageKey}. Error: ${err}`);
-            }
-            else {
-                const messageID = `message:${num}`;
+        try {
+            const num = await client.incr(messageKey);
+            const messageID = `message:${num}`;
 
+            try {
                 // Add to a sorted list by the time our message
-                client.ZADD(messageList, time, messageID, (err, redisResponse) => {
-                    if (err) {
-                        responseError(res, `Redis: couldn't add to sorted list "${messageList}". Error: ${err}`);
-                    }
-                    else if (!redisResponse){
-                        // Note: this error can't happen from cross servers, only by changing the redis manually
-                        responseError(res, `Redis: couldn't add to sorted list "${messageList}". Duplicate ${messageID}`);
-                    }
-                    else {
-                        // Message was added to sorted list, save message
-                        client.set(messageID, message);
+                const redisResponse = await client.zadd(messageList, time, messageID);
 
-                        res.send(JSON.stringify({ status: 'ok', messageID }));
-                    }
-                });
+                if (!redisResponse) {
+                    throw "Redis failed excecuting zadd";
+                }
 
+                try {
+                    // Message was added to sorted list, save message
+                    await client.set(messageID, message);
+                    res.send(JSON.stringify({ status: 'ok', messageID }));
+                }
+                catch(e) {
+                    responseError(res, `Redis: couldn't add set "${messageList}". Error: ${e}`);
+                }
             }
-        });
+            catch(e) {
+                responseError(res, `Redis: couldn't add to sorted list "${messageList}". Error: ${e}`);
+            }
+        }
+        catch(e) {
+            responseError(res, `Redis: couldn't increment ${messageKey}. Error: ${err}`);
+        }
     }
 });
 
