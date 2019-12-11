b5083cf204f98309c1ac7d1494c68b4f91a3cdec
Michael Tsafrir <michaeltsafrir@gmail.com>
2019-12-11 04:20:31 +0200

    switched fetchMessage to async

--- index.js
+++ index.js
@@ -18,52 +18,65 @@ const client = new ioredis(redisPort, redisHost);
 const responseError = (response, message) => response.send(JSON.stringify({ status: 'error', message }));
 
 // Get the earliest message that exists
-// const fetchMessage = () => client.zrange(messageList, 0, 0, 'withscores', (err, messageSet) => {
-//     if (err) {
-//         return console.error(`Redis: error when fetching first element from "${messageList}". Error: ${err}`);
-//     }
-//     else if (!messageSet.length) {
-//         // No message fetched
-//         return;
-//     }
-//     else {
-//         // Fetched message
-//         const messageID = messageSet[0];
-//         const messageTime = parseInt(messageSet[1]);
-//         const now = Date.now() / 1000; // Get epoch time in seconds
+const fetchMessage = async () => {
+    try {
+        const messageSet = await client.zrange(messageList, 0, 0, 'withscores');
 
-//         if (messageTime <= now) {
-//             client.get(messageID, (err, message) => {
-//                 if (err) {
-//                     return console.error(`Redis: error when fetching "${messageID}". Error: ${err}`);
-//                 }
-//                 else {
-//                     console.log(message);
+        if (!messageSet.length) {
+            // No message fetched
+            return;
+        }
+        else {
+            // Fetched message
+            const messageID = messageSet[0];
+            const messageTime = parseInt(messageSet[1]);
+            const now = Date.now() / 1000; // Get epoch time in seconds
 
-//                     // Delete message
-//                     client.zrem(messageList, messageID, (err, wasRemoved) => {
-//                         if (err || !wasRemoved) {
-//                             console.error(`Redis: Couldn't delete "${messageID}" from "${messageList}". Error: ${err}`);
-//                         }
-//                         else {
-//                             client.del(messageID, (err, wasRemoved) => {
-//                                 if (err || !wasRemoved) {
-//                                     console.error(`Redis: Couldn't delete "${messageID}". Error: ${err}`);
-//                                 }
-//                             });
-//                         }
-//                     });
+            if (messageTime <= now) {
+                try {
+                    const message = await client.get(messageID);
 
-//                     // Check next message
-//                     return fetchMessage();
-//                 }
-//             });
-//         }
-//     }
-// });
+                    console.log(message);
+
+                    try {
+                        // Delete message
+                        const wasRemoved = await client.zrem(messageList, messageID);
+
+                        if (!wasRemoved) {
+                            throw "Redis failed zrem operaion";
+                        }
+
+                        try {
+                            const wasRemoved = await client.del(messageID);
+
+                            if (!wasRemoved) {
+                                throw "Redis failed del operaion";
+                            }
+                        }
+                        catch(e) {
+                            console.error(`Redis: Couldn't delete "${messageID}". Error: ${err}`);
+                        }
+
+                        // Check next message
+                        return fetchMessage();
+                    }
+                    catch(e) {
+                        return console.error(`Redis: Couldn't delete "${messageID}" from "${messageList}". Error: ${e}`);
+                    }
+                }
+                catch(e) {
+                    return console.error(`Redis: error when fetching "${messageID}". Error: ${err}`);
+                }
+            }
+        }
+    }
+    catch(e) {
+        return console.error(`Redis: error when fetching first element from "${messageList}". Error: ${e}`);
+    }
+};
 
 // Run from the earliest message that exists
-// fetchMessage();
+fetchMessage();
 
 // Use bodyParser Middleware to fetch body params
 app.use(bodyParser.urlencoded({ extended: false }));
